bdead1d4405baa0bff47896327f7a72694c9c758
NeekoKun <leonardo.riccim@gmail.com>
2025-03-31 22:26:27 +0200

    Working with KDTree (SciPy)

--- simulation_no_grid.py
+++ simulation_no_grid.py
@@ -1,10 +1,8 @@
 from utils import Ant
+from scipy.spatial import KDTree
 import numpy as np
 import pygame
-import random
 import logging
-import multiprocessing
-import concurrent.futures
 import os
 
 class ColonySim:
@@ -12,49 +10,82 @@ class ColonySim:
         self.frame_count = 0
         self.output_dir = "frames"
         os.makedirs(self.output_dir, exist_ok=True)
+        
         self.map_size = (1000, 1000)
+        
         self.colony_location = colony_location if colony_location else (500, 500)
         self.colony_radius = 10
+        
         self.food_radius = food_radius if food_radius else 6
         self.food_locations = food_locations if food_locations else [(200, 200)]
+        
         self.home_pheromones = {}
         self.food_pheromones = {}
 
+        self.home_pheromones_tree = KDTree(np.empty((0, 2)))
+        self.food_pheromones_tree = KDTree(np.empty((0, 2)))
 
         pygame.display.init()
         self.screen = pygame.display.set_mode(self.map_size)
         self.clock = pygame.time.Clock()
 
-        self.ants = [Ant(self.colony_location) for _ in range(ants)]
-
-    def process_ant(self, ant):
-        result = ant.move(self.food_locations, self.food_radius, self.colony_location, self.colony_radius, self.map_size, self.home_pheromones, self.food_pheromones)
-        return ant, result
+        self.ant_vision_radius = 50
+        self.ants = [Ant(self.colony_location, vision_radius=self.ant_vision_radius) for _ in range(ants)]
     
+        self.pheromone_depletion_function = lambda x: 10*(1 - 1/(1 + np.exp((40 - x)/10)))
+
+    def update_pheromone_trees(self):
+        home_pheromone_locations = np.array(list(self.home_pheromones.keys()))
+        food_pheromone_locations = np.array(list(self.food_pheromones.keys()))
+
+        if len(home_pheromone_locations) > 0:
+            self.home_pheromones_tree = KDTree(home_pheromone_locations)
+        else:
+            self.home_pheromones_tree = KDTree(np.empty((0, 2)))
+
+        if len(food_pheromone_locations) > 0:
+            self.food_pheromones_tree = KDTree(food_pheromone_locations)
+        else:
+            self.food_pheromones_tree = KDTree(np.empty((0, 2)))
+
     def iterate_simulation(self):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
-            results = list(executor.map(self.process_ant, self.ants))
-
-        for ant, result in results:
-            if result == "Collected food":
-                logging.info(f"Ant {ant} collected food at {ant.location}")
-            elif result == "Deposited food":
-                logging.info(f"Ant {ant} deposited food at {ant.location}")
-            else:
-                if ant.has_food:
-                    self.food_pheromones[ant.location] = 10
+        ## Move ants
+        for ant in self.ants:
+            if ant.has_food:
+                if len(self.home_pheromones) == 0:
+                    pheromones = []
                 else:
-                    self.home_pheromones[ant.location] = 10
+                    pheromones_indices = self.home_pheromones_tree.query_ball_point(ant.location, self.ant_vision_radius)
+                    pheromones = [((tuple(self.home_pheromones_tree.data[i])), self.home_pheromones[tuple(self.home_pheromones_tree.data[i])]) for i in pheromones_indices]
+                ant.move(self.food_locations, self.food_radius, self.colony_location, self.colony_radius, self.map_size, pheromones)
 
+            else:
+                if len(self.food_pheromones) == 0:
+                    pheromones = []
+                else:
+                    pheromones_indices = self.food_pheromones_tree.query_ball_point(ant.location, self.ant_vision_radius)
+                    pheromones = [((tuple(self.food_pheromones_tree.data[i])), self.food_pheromones[tuple(self.food_pheromones_tree.data[i])]) for i in pheromones_indices]
+                ant.move(self.food_locations, self.food_radius, self.colony_location, self.colony_radius, self.map_size, pheromones)
+
+        ## Update pheromones
+        for ant in self.ants:
+            if ant.has_food:
+                self.food_pheromones[ant.location] = self.pheromone_depletion_function(ant.steps)
+            else:
+                self.home_pheromones[ant.location] = self.pheromone_depletion_function(ant.steps)
+
+        ## Dacay pheromones
         for home_pheromone in list(self.home_pheromones.keys()):
-            self.home_pheromones[home_pheromone] -= 0.1
+            self.home_pheromones[home_pheromone] *= 0.99
             if self.home_pheromones[home_pheromone] <= 0:
                 del self.home_pheromones[home_pheromone]
         
         for food_pheromone in list(self.food_pheromones.keys()):
-            self.food_pheromones[food_pheromone] -= 0.1
+            self.food_pheromones[food_pheromone] *= 0.99
             if self.food_pheromones[food_pheromone] <= 0:
                 del self.food_pheromones[food_pheromone]
+
+        self.update_pheromone_trees()
 
     def display(self):
         self.screen.fill((7, 7, 9))
@@ -63,18 +94,19 @@ class ColonySim:
         for food in self.food_locations:
             pygame.draw.circle(self.screen, (255, 0, 0), food, self.food_radius)
         
-        """
+        
         for home_pheromone in self.home_pheromones:
             pygame.draw.circle(self.screen, (0, 0, 255), home_pheromone, self.home_pheromones[home_pheromone] / 3)
         for food_pheromone in self.food_pheromones:
             pygame.draw.circle(self.screen, (255, 0, 255), food_pheromone, self.food_pheromones[food_pheromone] / 3)
-    "   """
+       
 
         for ant in self.ants:
             if ant.has_food:
                 pygame.draw.circle(self.screen, (255, 255, 0), ant.location, 2)
             else:
                 pygame.draw.circle(self.screen, (100, 100, 100), ant.location, 2)
+        
         pygame.image.save(self.screen, f"{self.output_dir}/frame_{self.frame_count:05d}.png")
         self.frame_count += 1
 
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import random
 
 class Ant:
     def __init__(self, coordinates, rotation=None, speed=10, vision_radius=50, radius=5):
-        
+        self.steps = 0
         self.location = coordinates
         self.has_food = False
         self.vision_radius = vision_radius
@@ -14,11 +14,16 @@ class Ant:
     def distance(self, other):
         return np.sqrt((self.location[0] - other[0]) ** 2 + (self.location[1] - other[1]) ** 2)
 
-    def move(self, food, food_radius, colony, colony_radius, grid_size, home_pheromones, food_pheromones):
+    def move(self, food, food_radius, colony, colony_radius, grid_size, pheromones: list[list[np.float64], float]):
+        """
+        "Move the ant towards food or colony based on its current state."
+        """
         ## Rotate randomly
         if random.random() < 0.1:  # 10% chance to rotate randomly
             self.rotation += random.uniform(-np.pi / 4, np.pi / 4)
             self.rotation %= (2 * np.pi)
+        
+        self.steps += 1
 
         ## Check if on food or on colony
         if not self.has_food:
@@ -26,48 +31,64 @@ class Ant:
                 if self.distance(food_location) < food_radius + self.radius:
                     self.has_food = True
                     self.rotation += np.pi
+                    self.steps = 0
                     return "Collected food"
         else:
             if self.distance(colony) < colony_radius + self.radius: 
                 self.has_food = False
                 self.rotation += np.pi
+                self.steps = 0
                 return "Deposited food"
 
         ## Check if food or colony in FOV
-        if not self.has_food:
+        targets = []
+        targets_weight = []
+
+        if not self.has_food: # Looking for food
+            for location, strenght in pheromones:
+                angle_to_food_pheromone = np.arctan2(location[1] - self.location[1], location[0] - self.location[0])
+                angle_diff = (angle_to_food_pheromone - self.rotation + np.pi) % (2 * np.pi) - np.pi
+                if abs(angle_diff) > np.pi / 4:
+                    continue
+                else:
+                    targets.append(tuple(location))
+                    targets_weight.append(strenght)
+
             for food_location in food:
                 if self.distance(food_location) < self.vision_radius:
                     angle_to_food = np.arctan2(food_location[1] - self.location[1], food_location[0] - self.location[0])
                     angle_diff = (angle_to_food - self.rotation + np.pi) % (2 * np.pi) - np.pi
                     if abs(angle_diff) > np.pi / 4:  # Assuming a 90-degree cone of vision
                         continue
-                    self.rotation = np.arctan2(food_location[1] - self.location[1], food_location[0] - self.location[0])
-                    break
-            else:
-                for food_pheromone in food_pheromones:
-                    if self.distance(food_pheromone) < self.vision_radius:
-                        angle_to_food_pheromone = np.arctan2(food_pheromone[1] - self.location[1], food_pheromone[0] - self.location[0])
-                        angle_diff = (angle_to_food_pheromone - self.rotation + np.pi) % (2 * np.pi) - np.pi
-                        if abs(angle_diff) > np.pi / 4:
-                            continue
-                        self.rotation = np.arctan2(food_pheromone[1] - self.location[1], food_pheromone[0] - self.location[0])
-                        break
-        else:
+                    else:
+                        targets.append(tuple(food_location))
+                        targets_weight.append(100000)
+
+        else: # Looking for home
+            for location, strenght in pheromones:
+                angle_to_home_pheromone = np.arctan2(location[1] - self.location[1], location[0] - self.location[0])
+                angle_diff = (angle_to_home_pheromone - self.rotation + np.pi) % (2 * np.pi) - np.pi
+                if abs(angle_diff) > np.pi / 4:
+                    continue
+                else:
+                    targets.append(tuple(location))
+                    targets_weight.append(strenght)
+            
             if self.distance(colony) < self.vision_radius:
                 angle_to_colony = np.arctan2(colony[1] - self.location[1], colony[0] - self.location[0])
                 angle_diff = (angle_to_colony - self.rotation + np.pi) % (2 * np.pi) - np.pi
                 if abs(angle_diff) > np.pi / 4:
                     return
-                self.rotation = np.arctan2(colony[1] - self.location[1], colony[0] - self.location[0])
-            else:
-                for home_pheromone in home_pheromones:
-                    if self.distance(home_pheromone) < self.vision_radius:
-                        angle_to_home_pheromone = np.arctan2(home_pheromone[1] - self.location[1], home_pheromone[0] - self.location[0])
-                        angle_diff = (angle_to_home_pheromone - self.rotation + np.pi) % (2 * np.pi) - np.pi
-                        if abs(angle_diff) > np.pi / 4:
-                            continue
-                        self.rotation = np.arctan2(home_pheromone[1] - self.location[1], home_pheromone[0] - self.location[0])
-                        break
+                else:
+                    targets = [tuple(colony)]
+                    targets_weight = [1]
+
+        if len(targets) > 0:
+            total_weight = sum(targets_weight)
+            targets_weight = [weight / total_weight for weight in targets_weight]
+            target = targets[np.random.choice(len(targets), p=targets_weight)]
+
+            self.rotation = np.arctan2(target[1] - self.location[1], target[0] - self.location[0])
 
         ## Move in the current direction
         new_x = self.location[0] + self.speed * np.cos(self.rotation)
