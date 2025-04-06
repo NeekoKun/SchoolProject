31b4c964dfa3c9afc69a341dba4434e0ba96b1ab
NeekoKun <leonardo.riccim@gmail.com>
2025-04-06 23:13:15 +0200

    Added Pause Feature

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ RANGE = [10, 10]
 ORIGIN = [0, 0]
 PADDING = (1, 1)
 dt = 0.01
-reset_probability = 0.1
+reset_probability = 0
 
 for i in RANGE:
     if i < 0:
         raise ValueError("RANGE must be positive")
 
 ## Colors
-axes_color = (200, 200, 200)
+axes_color = (255, 255, 255, int(0.5 * 255))
 
 background_color = (10, 10, 10)
 point_base_color = (255, 0, 0)
@@ -29,7 +29,7 @@ def equation(x, y, dx, dy):
     global dt
     d2y = 0
     d2x = 0
-    dy = x
+    dy = x-y
     dx = -y
     dy += d2y * dt
     dx += d2x * dt
@@ -44,8 +44,11 @@ tick_font = pygame.font.SysFont('Arial', 15)
 point_count = 10000
 screen.fill(background_color)
 
+pause_button = pygame.Rect(WIDTH - 50, 10, 40, 40)
+
 background_layer = pygame.Surface(SIZE)
 axes_layer       = pygame.Surface(SIZE, pygame.SRCALPHA)
+tracking_layer   = pygame.Surface(SIZE, pygame.SRCALPHA)
 
 dimming_surface = pygame.Surface(SIZE, pygame.SRCALPHA)
 dimming_surface.fill((background_color[0], background_color[1], background_color[2], int(0.05 * 255)))
@@ -92,6 +95,8 @@ def display(in_points, new_in_points):
 def display_tracking_points(in_tracking_points):
     global background_layer
 
+    tracking_layer.fill((0, 0, 0, 0))
+
     for tracking_point in in_tracking_points:
         for i in range(len(tracking_point) - 1):
             coors1 = [(tracking_point[i][0]       - ORIGIN[0]) * WIDTH / RANGE[0], WIDTH - (tracking_point[i][1]       - ORIGIN[1]) * HEIGHT / RANGE[1]]
@@ -99,7 +104,7 @@ def display_tracking_points(in_tracking_points):
             speed = np.sqrt((coors1[0] - coors2[0])**2 + (coors1[1] - coors2[1])**2)
             point = (int(coors1[0]), int(coors1[1]))
             new_point = (int(coors2[0]), int(coors2[1]))
-            pygame.draw.line(background_layer, (100, 200, 100), point, new_point, max(int(speed), 1))
+            pygame.draw.line(tracking_layer, (100, 200, 100), point, new_point, max(int(speed), 1))
 
 def coors_to_screen(coors):
     coors = np.array(coors)
@@ -143,7 +148,7 @@ def display_axes():
         coors = coors_to_screen([i, 0])
         pygame.draw.line(axes_layer, axes_color, (coors[0], min(max(coors[1], 0), HEIGHT)  - x_ax_tick_thickness), (coors[0], min(max(coors[1], 0), HEIGHT) + x_ax_tick_thickness), 2)
         if i != 0:
-            text = tick_font.render(str(i), True, axes_color)
+            text = tick_font.render(str(int(i)), True, axes_color)
             text_rect = text.get_rect(center=(coors[0], coors[1] + 10))
             if ax_height < HEIGHT / 2:
                 axes_layer.blit(text, (coors[0] - text_rect.width / 2, ax_height + 30))
@@ -154,7 +159,7 @@ def display_axes():
         coors = coors_to_screen([0, i])
         pygame.draw.line(axes_layer, axes_color, (min(max(coors[0], 0), WIDTH) - y_ax_tick_thickness, coors[1]), (min(max(coors[0], 0), WIDTH)  + y_ax_tick_thickness, coors[1]), 2)
         if i != 0:
-            text = tick_font.render(str(i), True, axes_color)
+            text = tick_font.render(str(int(i)), True, axes_color)
             text_rect = text.get_rect(center=(coors[0] + 10, coors[1]))
             if ax_width > WIDTH / 2:
                 axes_layer.blit(text, (ax_width - text_rect.width - 20, coors[1] - text_rect.height / 2))
@@ -179,6 +184,7 @@ def add_point(x, y):
 x0, y0 = 0, 0
 x_shift, y_shift = 0, 0
 moved = True
+paused = False
 
 while True:
     for event in pygame.event.get():
@@ -186,14 +192,16 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
+            if event.button == 1 and pause_button.collidepoint(event.pos):
+                paused = not paused
+            elif event.button == 1:
                 x, y = pygame.mouse.get_pos()
                 x = (x / WIDTH) * RANGE[0] + ORIGIN[0]
                 y = ((HEIGHT - y) / HEIGHT) * RANGE[1] + ORIGIN[1]
                 add_point(x, y)
-            if event.button == 2: # Middle Mouse Button: Panning
+            if event.button == 2 and not paused: # Middle Mouse Button: Panning
                 x0, y0 = pygame.mouse.get_pos()
-            if event.button == 4: # Scroll Up: Zoom in
+            if event.button == 4 and not paused: # Scroll Up: Zoom in
                 """
                 Zoom In:
                 The Range beecomes 90% of the original range
@@ -203,7 +211,7 @@ while True:
                 ORIGIN[1] += (RANGE[1] / 10) * ((HEIGHT - zoom_y) / HEIGHT)
                 RANGE = [RANGE[0] * 0.9, RANGE[1] * 0.9]
                 moved = True
-            if event.button == 5: # Scroll Down: Zoom out
+            if event.button == 5 and not paused: # Scroll Down: Zoom out
                 """
                 Zoom In:
                 The Range beecomes 110% of the original range
@@ -213,10 +221,10 @@ while True:
                 ORIGIN[1] -= (RANGE[1] / 10) * ((HEIGHT - zoom_y) / HEIGHT)
                 RANGE = [RANGE[0] * 1.1, RANGE[1] * 1.1]
                 moved = True
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP and not paused:
             if event.button == 2:
                 x0, y0 = 0, 0
-        if pygame.mouse.get_pressed()[1]: # LMB + Shift: Rudimentary panning
+        if pygame.mouse.get_pressed()[1] and not paused: # LMB + Shift: Rudimentary panning
             x, y = pygame.mouse.get_pos()
             if x0 != 0 or y0 != 0:
                 x_shift = (x - x0) / WIDTH * RANGE[0]
@@ -225,26 +233,29 @@ while True:
             x0, y0 = pygame.mouse.get_pos()
             moved = True
 
-    new_points = update(points)
-    tracking_points = update_tracking_points(tracking_points)
+    if not paused:
+        new_points = update(points)
+        tracking_points = update_tracking_points(tracking_points)
 
-    # Check if points are out of bounds
-    for i in range(point_count):
-        if points[i][0] < ORIGIN[0] - PADDING[0] or points[i][0] > ORIGIN[0] + RANGE[1] + PADDING[0]  or points[i][1] < ORIGIN[1] - PADDING[1] or points[i][1] > ORIGIN[1] + RANGE[1] + PADDING[1] or np.random.rand() < reset_probability:
-            coors = [np.random.rand() * (RANGE[0] + 2 * PADDING[0]) + (ORIGIN[0] - PADDING[0]), np.random.rand() * (RANGE[1] + 2 * PADDING[1]) + (ORIGIN[1] - PADDING[1]), 0, 0]
-            points[i] = coors
-            new_points[i] = coors
+        # Check if points are out of bounds
+        for i in range(point_count):
+            if points[i][0] < ORIGIN[0] - PADDING[0] or points[i][0] > ORIGIN[0] + RANGE[1] + PADDING[0]  or points[i][1] < ORIGIN[1] - PADDING[1] or points[i][1] > ORIGIN[1] + RANGE[1] + PADDING[1] or np.random.rand() < reset_probability:
+                coors = [np.random.rand() * (RANGE[0] + 2 * PADDING[0]) + (ORIGIN[0] - PADDING[0]), np.random.rand() * (RANGE[1] + 2 * PADDING[1]) + (ORIGIN[1] - PADDING[1]), 0, 0]
+                points[i] = coors
+                new_points[i] = coors
 
-    # Display the points
-    display(points, new_points)
-    display_tracking_points(tracking_points)
-    if moved:
-        display_axes()
-        moved = False
+        # Display the points
+        display(points, new_points)
+        display_tracking_points(tracking_points)
+        if moved:
+            display_axes()
+            moved = False
 
     # Blit background and Axix
     screen.blit(background_layer, (0, 0))
     screen.blit(axes_layer, (0, 0))
+    screen.blit(tracking_layer, (0, 0))
+    pygame.draw.rect(screen, (200, 200, 200), pause_button) if paused else pygame.draw.rect(screen, (100, 100, 100), pause_button)
 
     points = np.array(new_points)
 
